02f742bc5da17b66fa3692a9e78c8a467d3f9efa
Ayman Choughari <ayman.choughari@gmail.com>
2021-12-12 10:35:47 +0000

    Clean script code

--- script.js
+++ script.js
@@ -4,83 +4,70 @@ function printData(data,containerName) {
     }
 const buttons=document.querySelectorAll("button");
 buttons.forEach((button) => {
+    button.addEventListener("click",clearPrintOutcome);
     button.addEventListener("click",game);
-    
-    //button.addEventListener("click", printOutcome(result));
-   // button.addEventListener("click",e => console.log(game(e)));
     });
-    //function game(numberRounds=3){
-    function game(evt){
-    let playerScore =0;
-    let computerScore =0;
-   // for (round=1;round<=numberRounds;round++){
 
-        let playerSelection=evt.target.textContent;
-        console.log(evt.target.textContent)
-        let computerSelection=computerPlay();
-        let roundResult=gameRound(playerSelection,computerSelection)
-        switch(roundResult) {   
-            case 1: playerScore=1
-                    break;
-            case -1:computerScore=1
-                    break;
+    function computerSelection(){ 
+        //Randomized CPU play: If 0 return rock, if 1 return paper else return scissors
+        let randomOutcome=Math.floor(Math.random()*3)            
+        if (randomOutcome===0) {
+            return "rock";
         }
-        
-    //}
-    let outcome={"score":[playerScore,computerScore],"playerSelection":playerSelection,"computerSelection":computerSelection};
-    printOutcome(outcome)
-    //return outcome
+        else if (randomOutcome===1) {
+            return "paper";
+        }
+        else {
+            return "scissors";
+        }
+    }
+
+function game(evt){
+    let playerSelection=evt.target.textContent;
+    let computerPlay=computerSelection();
+    let roundOutcome={"score":roundResult(playerSelection,computerPlay),"playerSelection":playerSelection,"computerSelection":computerPlay};
+    printOutcome(roundOutcome)
 }
 
-function printOutcome(game){
-    //printData(`****** Welcome to round ${round} ******`,".roundStartInfo");
-    //printData(roundOutcome(roundResult,playerSelection,computerSelection),".outcomeInfo");
-    
-    printData(`human played ${game.playerSelection} and scored ${game.score[0]}`,"#playerScore"); 
-    printData(`CPU played ${game.computerSelection} and scored ${game.score[1]}`,"#computerScore"); 
-    
+function printOutcome(roundOutcome){
+    if(roundOutcome.score[0]===1) {
+        printData("Congratulations, you won!!",".outcomeInfo"); 
+        }
+    else if (roundOutcome.score[1]===1) {
+        printData("Uh oh, CPU won...",".outcomeInfo");   
+        }
+    else{
+        printData("It's a draw!!!",".outcomeInfo"); 
+        return
+    }
+    printData(`****** Welcome to round 1 ******`,".roundNumber");
+    printData(`human played ${roundOutcome.playerSelection} and scored ${roundOutcome.score[0]}`,"#playerScore"); 
+    printData(`CPU played ${roundOutcome.computerSelection} and scored ${roundOutcome.score[1]}`,"#computerScore"); 
     //printData(` ****** End of round ${round} out of ${numberRounds} ******`,".roundEndInfo");
+}
 
+function clearPrintOutcome(){
+    printData('',".outcomeInfo");
+    printData('',"#playerScore");
+    printData('',"#computerScore");
 }
-function computerPlay(){ 
-    //Randomized CPU play: If 0 return rock, if 1 return paper else return scissors
-    let randomOutcome=Math.floor(Math.random()*3)            
-    if (randomOutcome===0) {
-        return "rock";
-    }
-    else if (randomOutcome===1) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-function gameRound(playerSelection,computerSelection){
+
+function roundResult(playerSelection,computerSelection){
     //Determines the outcome of a round of rock, paper scissors. 
     //If user wins +1, if user looses -1, if draw 0
     gameHierarchy={"rock":"scissors","paper":"rock","scissors":"paper"};
     if (playerSelection===computerSelection){
-        return 0;
+        return [0,0];
     }
     if (gameHierarchy[playerSelection]===computerSelection){
-        return 1;
+        return [1,0];
     }
     else {
-        return -1;
+        return [0,1];
     } 
 }
 
-// function roundOutcome(gameRound,playerSelection,computerSelection){
-//     if (gameRound===1){
-//         return `You win, ${playerSelection} beat ${computerSelection}`;
-//     } 
-//     else if (gameRound===-1){
-//         return `You loose, ${computerSelection} beat ${playerSelection}`;
-//     }
-//     else {
-//         return `It's a draw, both of you played ${playerSelection}`;
-//     }
-// }
+
 
 
 
@@ -114,5 +101,17 @@ function lowerCaseInput(playerSelection){
 //     }
 //     else {
 //         return `Computer wins, the final score is: HUM ${playerScore} - CPU ${computerScore}`;
+//     }
+// }
+
+// function roundOutcome(gameRound,playerSelection,computerSelection){
+//     if (gameRound===1){
+//         return `You win, ${playerSelection} beat ${computerSelection}`;
+//     } 
+//     else if (gameRound===-1){
+//         return `You loose, ${computerSelection} beat ${playerSelection}`;
+//     }
+//     else {
+//         return `It's a draw, both of you played ${playerSelection}`;
 //     }
 // }
